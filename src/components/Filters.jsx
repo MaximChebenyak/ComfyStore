a1f998cb85e082aa6ec6a4c6537a121301828363
maxim <maximchebenyak@gmail.com>
2025-05-27 13:36:14 +0300

    filters params

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,7 +5,8 @@ import FormRange from './FormRange';
 import FormCheckbox from './FormCheckbox';
 
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
@@ -13,6 +14,7 @@ const Filters = () => {
         type="search"
         label="search product"
         name="search"
+        defaultValue={search}
         size="input-sm"
       />
       {/* CATEGORIES */}
@@ -20,6 +22,7 @@ const Filters = () => {
         label="select category"
         name="category"
         list={meta.categories}
+        defaultValue={category}
         size="select-sm"
       />
       {/* COMPANIES */}
@@ -27,6 +30,7 @@ const Filters = () => {
         label="select company"
         name="company"
         list={meta.companies}
+        defaultValue={company}
         size="select-sm"
       />
       {/* ORDER */}
@@ -34,14 +38,25 @@ const Filters = () => {
         label="sort by"
         name="order"
         list={['a-z', 'z-a', 'high', 'low']}
+        defaultValue={order}
         size="select-sm"
       />
       {/* PRICE */}
-      <FormRange label="select price" name="price" size="range-sm" />
+      <FormRange
+        label="select price"
+        name="price"
+        price={price}
+        size="range-sm"
+      />
       {/* SHIPPING */}
-      <FormCheckbox label="free shipping" name="shipping" size="checkbox-sm" />
+      <FormCheckbox
+        label="free shipping"
+        name="shipping"
+        defaultValue={shipping}
+        size="checkbox-sm"
+      />
       {/* BUTTONS */}
-      <button type="submit" className="btn btn-primary btn-sm ">
+      <button type="submit" className="btn btn-primary btn-sm">
         search
       </button>
       <Link to="/products" className="btn btn-accent btn-sm">
