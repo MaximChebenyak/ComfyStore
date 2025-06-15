473ca06d21f794002cba2ac5fa51a06834e0700e
maxim <maximchebenyak@gmail.com>
2025-06-15 11:58:39 +0300

    checkout page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,25 @@
-import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+/* import { FormInput, SubmitBtn } from '../components';
+import { Form, Link, redirect } from 'react-router-dom';
+
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    const response = await customFetch.post('/auth/local/register', data);
+    toast.success('account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      'please double check your credentials';
+
+    toast.error(errorMessage);
+    return null;
+  }
+};
 
 const Register = () => {
   return (
@@ -9,9 +29,92 @@ const Register = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Register</h4>
-        <FormInput type="text" label="username" name="username" />
-        <FormInput type="email" label="email" name="email" />
-        <FormInput type="password" label="password" name="password" />
+        <FormInput
+          type="text"
+          label="username"
+          name="username"
+          defaultValue="james1 smith"
+        />
+        <FormInput
+          type="email"
+          label="email"
+          name="email"
+          defaultValue="james1@test.com "
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          defaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="register" />
+        </div>
+
+        <p className="text-center">
+          Already a member?
+          <Link
+            to="/login"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
+};
+export default Register;
+ */
+
+import { FormInput, SubmitBtn } from '../components';
+import { Form, redirect, Link } from 'react-router-dom';
+
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    const response = await customFetch.post('/auth/local/register', data);
+    toast.success('account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      'please double check your credentials';
+
+    toast.error(errorMessage);
+    return null;
+  }
+};
+
+const Register = () => {
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="POST"
+        className="card w-96 py-8 px-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput
+          type="text"
+          label="username"
+          name="username"
+          //defaultValue="testuser"
+        />
+        <FormInput
+          type="email"
+          label="email"
+          name="email"
+          //defaultValue="testuser@test.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          //defaultValue="secret"
+        />
         <div className="mt-4">
           <SubmitBtn text="register" />
         </div>
