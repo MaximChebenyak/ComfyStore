a53ea8b17b0e138031f689a73b9b9c7fd981b1d8
maxim <maximchebenyak@gmail.com>
2025-06-17 15:16:21 +0300

    orders list

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,13 @@ import { ErrorElement } from './components';
 import { loader as landingLoader } from './pages/Landing';
 import { loader as singleProductLoader } from './pages/SingleProduct';
 import { loader as productsloader } from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as ordersLoader } from './pages/Orders';
 
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
+
 import { store } from './store';
 
 const router = createBrowserRouter([
@@ -60,10 +64,14 @@ const router = createBrowserRouter([
       {
         path: 'checkout',
         element: <Checkout />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store),
       },
       {
         path: 'orders',
         element: <Orders />,
+        //errorElement: <ErrorElement />,
+        loader: ordersLoader(store),
       },
     ],
   },
